1ca90d14e3acfdbeba9fe0be73ac311aa3165d98
xsv7y <taras.kutsyk.pm.2019@lpnu.ua>
2021-04-06 15:41:12 +0300

    contacts upd

--- app/js/contacts.js
+++ app/js/contacts.js
@@ -1,20 +1,63 @@
 import {MDCDataTable} from '@material/data-table';
 import User from './class/User';
 
-let form, submit;
+const myTable = new MDCDataTable(document.querySelector('.mdc-data-table'));
+
+const idGenerator = (function() {
+    let privateCounter = 0;
+
+    return {
+        next: function () {
+            return ++privateCounter;
+        }
+    };
+})();
+
+let form, submit, contacts;
 
 window.onload = () => {
+    contacts = document.getElementById('contactsBody');
     form = document.contactForm;
     submit = document.getElementById('add-contact-btn');
 
     submit.onclick = (ev) => {
         ev.preventDefault();
 
-        let {name, surname} = form;
-        console.log(name.value, surname.value);
-    }
+        const {name, surname} = form;
+        const newContact = formNewContact(name.value, surname.value);
 
-    const {adapter} = new MDCDataTable(document.querySelector('.mdc-data-table')).getDefaultFoundation();
-    // adapter.addClass('HELLO');
-    console.log(adapter)
+        contacts.appendChild(newContact);
+    }
+}
+
+function formNewContact(name, surname) {
+    const rowElement = document.createElement('tr');
+    rowElement.className = 'mdc-data-table__row';
+    rowElement.id = idGenerator.next();
+
+    const rowDataName = document.createElement('td');
+    const rowDataSurname = document.createElement('td');
+
+    rowDataName.className = 'mdc-data-table__cell mdc-data-table__cell--numeric';
+    rowDataSurname.className = 'mdc-data-table__cell mdc-data-table__cell--numeric';
+
+    rowDataName.innerText = name;
+    rowDataSurname.innerText = surname;
+
+    const deleteBtnWrapper = document.createElement('td');
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'mdc-button mdc-button--touch0 delete-contact-btn';
+    deleteBtn.innerHTML = '<span class="mdc-button__ripple"></span>\n' +
+        '                            <span class="mdc-button__label">\n' +
+        '                                <i class="material-icons mdc-list-item__graphic" aria-hidden="true">remove</i>\n' +
+        '                            </span>\n' +
+        '                            <span class="mdc-button__touch"></span>';
+
+    deleteBtnWrapper.appendChild(deleteBtn);
+
+    rowElement.appendChild(rowDataName);
+    rowElement.appendChild(rowDataSurname);
+    rowElement.appendChild(deleteBtnWrapper);
+
+    return rowElement;
 }
